40d003ee34b8decb54ed7a439195cb9e307997f2
Erwan <air01a@yahoo.fr>
2020-09-22 13:42:12 +0200

    service worker

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,11 +1,9 @@
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        fetch(event.request, {
-            mode: 'cors',
-            credentials: 'include',
-            headers: {
-                'test': 'Bearer mF_9.B5f-4.1JqM' // you would, of course, not hard-code this here...
-            }
-        })
-    )
-});
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+      caches.open('fox-store').then(function(cache) {
+        return cache.addAll([
+          ''
+        ]);
+      })
+    );
+   });
